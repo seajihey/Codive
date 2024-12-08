994f4470b7a9a9d7da2b6d21f552e098516711f6
seajihey <okcoco03@naver.com>
2024-12-09 03:24:25 +0900

    fiej

--- codive/codive.py
+++ codive/codive.py
@@ -16,7 +16,7 @@ room_users = {}
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:2000","http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://localhost:2000","http://127.0.0.1:3000","*"],
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],
