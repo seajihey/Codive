c86ed13161d6d102b7130b09af214414b70436d5
byabbya <borong39@gnu.ac.kr>
2024-12-08 22:05:39 +0900

    dd

--- codive/codive.py
+++ codive/codive.py
@@ -19,7 +19,7 @@ room_users = {}
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],
